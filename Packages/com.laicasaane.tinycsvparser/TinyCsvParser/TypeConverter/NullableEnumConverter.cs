c3e7b75f3ead6a7639530172c5542514b50d0a62
Laicasaane <laicasaane@outlook.com>
2020-12-03 11:14:14 +0700

    Add converter for nullable enums

---
+++ Packages/com.laicasaane.tinycsvparser/TinyCsvParser/TypeConverter/NullableEnumConverter.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace TinyCsvParser.TypeConverter
+{
+    public class NullableEnumConverter<TTargetType> : NullableInnerConverter<TTargetType>
+        where TTargetType : struct, Enum
+    {
+        public NullableEnumConverter()
+            : base(new EnumConverter<TTargetType>())
+        {
+        }
+
+        public NullableEnumConverter(bool ignoreCase)
+            : base(new EnumConverter<TTargetType>(ignoreCase))
+        {
+        }
+    }
+}
